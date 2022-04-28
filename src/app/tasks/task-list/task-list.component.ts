c998ea9aea9b8ce0c158875f2ab9a945f4f5c3b8
Danilooo99 <danilo.informatica.99@gmail.com>
2022-04-28 23:41:04 +0100

    TaskComplete and Delete

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../models/task.model';
 import { TaskService } from '../services/task.service';
-
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
@@ -9,6 +8,7 @@ import { TaskService } from '../services/task.service';
 })
 export class TaskListComponent implements OnInit {
 
+  flag = false;  
   Tasks: Task[];
 
   constructor(private taskService: TaskService) { }
@@ -26,5 +26,27 @@ export class TaskListComponent implements OnInit {
   }
 
   deleteRow = (task) => this.taskService.deleteTask(task);
+  
   displayedColumns: string[] = ['title', 'description', 'state', 'action'];
+
+  taskComplete(element){
+    var check = document.getElementById(element.id) as HTMLButtonElement;
+    var description = document.getElementById("desc"+element.id) as HTMLTableCellElement;
+    var title = document.getElementById("tit"+element.id) as HTMLTableCellElement;
+
+    if(check.innerText === "check"){
+      check.style.background = 'black';
+      check.innerHTML = "cancel";
+      description.style.textDecoration = "line-through";
+      title.style.textDecoration = "line-through";
+    } else {
+      check.style.background = "rgb(80, 70, 70)";
+      check.innerHTML = "check";
+      description.style.textDecoration = "none"; 
+      title.style.textDecoration = "none";
+    }
+  }
+
+  
+ 
 }
