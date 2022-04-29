65266d2b380f6fbdfc3b40bae04042b5f678bbda
Danilooo99 <danilo.informatica.99@gmail.com>
2022-04-29 21:39:03 +0100

    Netlify Configuration and small changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDrPrh2gvLO6SDotmJbEdYQzFrQSVbdyzA",
+    authDomain: "to-do-list-dawii.firebaseapp.com",
+    projectId: "to-do-list-dawii",
+    storageBucket: "to-do-list-dawii.appspot.com",
+    messagingSenderId: "144688615437",
+    appId: "1:144688615437:web:da841790bb559e22d88544",
+    measurementId: "G-BYVJCVMYLS"
+  }
 };
